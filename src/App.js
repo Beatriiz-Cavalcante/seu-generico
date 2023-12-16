1c2808ed8a37efaa6f304bb191aa12ffde6f2bd9
Neto <netoodeveloper@gmail.com>
2023-12-16 17:42:58 -0300

    Adicionando botão de pesquisa por voz

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Cabecalho from './components/Cabecalho';
 import Pesquisa from './components/Pesquisa';
+import PesquisaVoz from './components/PesquisaVoz';
 
 function App() {
   return (
     <main className="principal">
         <Cabecalho />
         <Pesquisa />
+        <PesquisaVoz />
     </main>
   );
 }
---
+++ src/components/PesquisaVoz.js
@@ -0,0 +1,15 @@
+import './PesquisaVoz.css';
+
+import Microfone from '../imagens/microfone.svg';
+
+function PesquisaVoz() {
+    return (
+        <div className='pesquisa-voz'>
+            <div className='.pesquisa-voz-container'>
+                <button className='pesquisa-voz-btn'><span>Pesquisa por voz</span><img src={Microfone} /></button>
+            </div>
+        </div>
+    )
+}
+
+export default PesquisaVoz;
