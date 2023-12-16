bff705b6bbdc2758586e016fb2a77873c9987bb9
Neto <netoodeveloper@gmail.com>
2023-12-16 16:36:39 -0300

    Criando Componentes Cabeçalho e Pesquisa

---
+++ src/components/Pesquisa.js
@@ -0,0 +1,19 @@
+import './Pesquisa.css';
+
+import magnify from '../imagens/search-magnify.svg'
+
+
+function Pesquisa() {
+    return (
+        <div className="pesquisa">
+            <div className="container-pesquisa">
+                <div className="input-wrapper">
+                    <input id='medicamento' className='barra-pesquisa' type='text' placeholder='Pesquisar medicamento' />
+                    <span className='input-img'><img src={magnify} alt='Pesquisar' /></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Pesquisa;
