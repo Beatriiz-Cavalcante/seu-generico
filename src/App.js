e483b06fe097fc009d683c347c12ef571e2f9ad9
Neto <netoodeveloper@gmail.com>
2023-12-17 00:50:30 -0300

    Adicionando mais Categorias e Carrosséis

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Pesquisa from './components/Pesquisa';
 import PesquisaVoz from './components/PesquisaVoz';
 import CategoriaNome1 from './components/CategoriaNome1';
 import Carrossel1 from './components/Carrossel1';
+import CategoriaNome2 from './components/CategoriaNome2';
+import Carrossel2 from './components/Carrossel2';
+import CategoriaNome3 from './components/CategoriaNome3';
+import Carrossel3 from './components/Carrossel3';
+
 
 function App() {
   return (
@@ -14,6 +19,10 @@ function App() {
         <PesquisaVoz />
         <CategoriaNome1 />
         <Carrossel1 />
+        <CategoriaNome2 />
+        <Carrossel2 />
+        <CategoriaNome3 />
+        <Carrossel3 />
     </main>
   );
 }
